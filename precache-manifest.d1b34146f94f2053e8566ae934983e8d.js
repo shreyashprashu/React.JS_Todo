b78dd725d81dca59dacd02b912d6b230f9193262
shreyash prashu <shreyashprashu93@gmail.com>
2020-07-26 04:32:52 +0530

    Updates

---
+++ precache-manifest.d1b34146f94f2053e8566ae934983e8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f3408ace87de995f09be7fa48eb747",
+    "url": "/React.JS_Todo/index.html"
+  },
+  {
+    "revision": "58b46713c2412ea16795",
+    "url": "/React.JS_Todo/static/css/main.8940aa77.chunk.css"
+  },
+  {
+    "revision": "592fd91d3172607bf969",
+    "url": "/React.JS_Todo/static/js/2.3e89c08b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React.JS_Todo/static/js/2.3e89c08b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b46713c2412ea16795",
+    "url": "/React.JS_Todo/static/js/main.9f5a5536.chunk.js"
+  },
+  {
+    "revision": "f10d850c8930def52e80",
+    "url": "/React.JS_Todo/static/js/runtime-main.2a16857e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React.JS_Todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
